6bd58e60d4bad803b758dbfecb422bbfa0dfa2ce
magic <huronghai2008@gmail.com>
2013-07-07 12:25:49 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickOrdering::Application.config.secret_token = 'a46005fb682e28b762657df7e75215ab7c1823974bcbc40f774bb72fba6bd4680e85e3b72caa21a5e1c4d6ee1c292b52f1d472bc628c77d8014e05c610b818ca'
