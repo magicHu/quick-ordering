362c3ef3675610bfc41c852d7e045d6dd438fa02
magic <huronghai2008@gmail.com>
2013-07-15 00:53:53 +0800

    remove scope validate

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ActiveRecord::Base
   attr_accessible :category_id, :desc, :name, :price, :restaurant_id
 
   validates :name, :desc, :price, :presence => true
-  validates :name, :uniqueness => { scope: :restaurant_id }
+  #validates :name, :uniqueness => { scope: :restaurant_id }
 
   belongs_to :restaurant
   belongs_to :category
